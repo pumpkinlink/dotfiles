20d691764e954de694ac2eac4c6816341551c847
Denis Franco <11864620+pumpkinlink@users.noreply.github.com>
2021-05-17 14:11:59 -0300

    fix surfingkeys theme

--- surfinkeys.js
+++ surfinkeys.js
@@ -13,46 +13,239 @@
 
 map('<Alt-S>','<Alt-s>');
 
-// set theme
+// ---- Hints ----
+// Hints have to be defined separately
+// Uncomment to enable
+
+// Tomorrow-Night
+/* -- DELETE LINE TO ENABLE THEME
+Hints.style('border: solid 2px #373B41; color:#52C196; background: initial; background-color: #1D1F21;');
+Hints.style("border: solid 2px #373B41 !important; padding: 1px !important; color: #C5C8C6 !important; background: #1D1F21 !important;", "text");
+Visual.style('marks', 'background-color: #52C19699;');
+Visual.style('cursor', 'background-color: #81A2BE;');
+-- DELETE LINE TO ENABLE THEME */
+
+// Nord
+Hints.style('border: solid 2px #4C566A; color:#A3BE8C; background: initial; background-color: #3B4252;');
+Hints.style("border: solid 2px #4C566A !important; padding: 1px !important; color: #E5E9F0 !important; background: #3B4252 !important;", "text");
+Visual.style('marks', 'background-color: #A3BE8C99;');
+Visual.style('cursor', 'background-color: #88C0D0;');
+
+// Doom One
+/* -- DELETE LINE TO ENABLE THEME
+Hints.style('border: solid 2px #282C34; color:#98be65; background: initial; background-color: #2E3440;');
+Hints.style("border: solid 2px #282C34 !important; padding: 1px !important; color: #51AFEF !important; background: #2E3440 !important;", "text");
+Visual.style('marks', 'background-color: #98be6599;');
+Visual.style('cursor', 'background-color: #51AFEF;');
+-- DELETE LINE TO ENABLE THEME */
+
+// Monokai
+/* -- DELETE LINE TO ENABLE THEME
+Hints.style('border: solid 2px #2D2E2E; color:#F92660; background: initial; background-color: #272822;');
+Hints.style("border: solid 2px #2D2E2E !important; padding: 1px !important; color: #A6E22E !important; background: #272822 !important;", "text");
+Visual.style('marks', 'background-color: #A6E22E99;');
+Visual.style('cursor', 'background-color: #F92660;');
+-- DELETE LINE TO ENABLE THEME */
+
 settings.theme = `
+/* Edit these variables for easy theme making */
+:root {
+  /* Font */
+  --font: 'Input Sans Condensed', 'Source Code Pro', Ubuntu, sans;
+  --font-size: 13;
+  /* --font-weight: bold; */
+  /* -------------------- */
+  /* --      NORD      -- */
+  /* -------------------- */
+  --fg: #E5E9F0;
+  --bg: #3B4252;
+  --bg-dark: #2E3440;
+  --border: #4C566A;
+  --main-fg: #88C0D0;
+  --accent-fg: #A3BE8C;
+  --info-fg: #5E81AC;
+  --select: #4C566A;
+  /* Unused Alternate Colors */
+  /* --orange: #D08770; */
+  /* --red: #BF616A; */
+  /* --yellow: #EBCB8B; */
+}
+/* ---------- Generic ---------- */
 .sk_theme {
-    font-family: Input Sans Condensed, Charcoal, sans-serif;
-    font-size: 10pt;
-    background: #24272e;
-    color: #abb2bf;
+background: var(--bg);
+color: var(--fg);
+  background-color: var(--bg);
+  border-color: var(--border);
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+}
+input {
+  font-family: var(--font);
+  font-weight: var(--font-weight);
 }
 .sk_theme tbody {
-    color: #fff;
+  color: var(--fg);
 }
 .sk_theme input {
-    color: #d0d0d0;
+  color: var(--fg);
+}
+/* Hints */
+#sk_hints .begin {
+  color: var(--accent-fg) !important;
+}
+#sk_tabs .sk_tab {
+  background: var(--bg-dark);
+  border: 1px solid var(--border);
+}
+#sk_tabs .sk_tab_title {
+  color: var(--fg);
+}
+#sk_tabs .sk_tab_url {
+  color: var(--main-fg);
+}
+#sk_tabs .sk_tab_hint {
+  background: var(--bg);
+  border: 1px solid var(--border);
+  color: var(--accent-fg);
+}
+.sk_theme #sk_frame {
+  background: var(--bg);
+  opacity: 0.2;
+  color: var(--accent-fg);
+}
+/* ---------- Omnibar ---------- */
+/* Uncomment this and use settings.omnibarPosition = 'bottom' for Pentadactyl/Tridactyl style bottom bar */
+ .sk_theme#sk_omnibar {
+  width: 100%;
+  left: 0;
+} 
+.sk_theme .title {
+  color: var(--accent-fg);
 }
 .sk_theme .url {
-    color: #61afef;
+  color: var(--main-fg);
 }
 .sk_theme .annotation {
-    color: #56b6c2;
+  color: var(--accent-fg);
 }
 .sk_theme .omnibar_highlight {
-    color: #528bff;
+  color: var(--accent-fg);
 }
 .sk_theme .omnibar_timestamp {
-    color: #e5c07b;
+  color: var(--info-fg);
 }
 .sk_theme .omnibar_visitcount {
-    color: #98c379;
+  color: var(--accent-fg);
 }
-.sk_theme #sk_omnibarSearchResult>ul>li:nth-child(odd) {
-    background: #303030;
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+  background: var(--bg-dark);
 }
-.sk_theme #sk_omnibarSearchResult>ul>li.focused {
-    background: #3e4452;
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+  background: var(--border);
 }
-#sk_status, #sk_find {
-    font-size: 12pt;
+.sk_theme #sk_omnibarSearchArea {
+  border-top-color: var(--border);
+  border-bottom-color: var(--border);
 }
-#sk_status {
-    border: 1px solid #ff6;
+.sk_theme #sk_omnibarSearchArea input,
+.sk_theme #sk_omnibarSearchArea span {
+  font-size: var(--font-size);
+}
+.sk_theme .separator {
+  color: var(--accent-fg);
+}
+/* ---------- Popup Notification Banner ---------- */
+#sk_banner {
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+  background: var(--bg);
+  border-color: var(--border);
+  color: var(--fg);
+  opacity: 0.9;
+}
+/* ---------- Popup Keys ---------- */
+#sk_keystroke {
+  background-color: var(--bg);
+}
+.sk_theme kbd .candidates {
+  color: var(--info-fg);
+}
+.sk_theme span.annotation {
+  color: var(--accent-fg);
+}
+/* ---------- Popup Translation Bubble ---------- */
+#sk_bubble {
+  background-color: var(--bg) !important;
+  color: var(--fg) !important;
+  border-color: var(--border) !important;
+}
+#sk_bubble * {
+  color: var(--fg) !important;
+}
+#sk_bubble div.sk_arrow div:nth-of-type(1) {
+  border-top-color: var(--border) !important;
+  border-bottom-color: var(--border) !important;
+}
+#sk_bubble div.sk_arrow div:nth-of-type(2) {
+  border-top-color: var(--bg) !important;
+  border-bottom-color: var(--bg) !important;
+}
+/* ---------- Search ---------- */
+#sk_status,
+#sk_find {
+  font-size: var(--font-size);
+  border-color: var(--border);
+}
+.sk_theme kbd {
+  background: var(--bg-dark);
+  border-color: var(--border);
+  box-shadow: none;
+  color: var(--fg);
+}
+.sk_theme .feature_name span {
+  color: var(--main-fg);
+}
+/* ---------- ACE Editor ---------- */
+#sk_editor {
+  background: var(--bg-dark) !important;
+  height: 50% !important;
+  /* Remove this to restore the default editor size */
+}
+.ace_dialog-bottom {
+  border-top: 1px solid var(--bg) !important;
+}
+.ace-chrome .ace_print-margin,
+.ace_gutter,
+.ace_gutter-cell,
+.ace_dialog {
+  background: var(--bg) !important;
+}
+.ace-chrome {
+  color: var(--fg) !important;
+}
+.ace_gutter,
+.ace_dialog {
+  color: var(--fg) !important;
+}
+.ace_cursor {
+  color: var(--fg) !important;
+}
+.normal-mode .ace_cursor {
+  background-color: var(--fg) !important;
+  border: var(--fg) !important;
+  opacity: 0.7 !important;
+}
+.ace_marker-layer .ace_selection {
+  background: var(--select) !important;
+}
+.ace_editor,
+.ace_dialog span,
+.ace_dialog input {
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
 }
 `;
 // click `Save` button to make above settings to take effect.
